d1fd119375be592abb0632affcf2cad06865013e
myosung <rabitmyo@naver.com>
2023-12-04 23:48:08 +0900

    filebase install

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsaxOyodqLzDFyerytCpoPwQnV-fr3eak",
+  authDomain: "react-twt-a6a6f.firebaseapp.com",
+  projectId: "react-twt-a6a6f",
+  storageBucket: "react-twt-a6a6f.appspot.com",
+  messagingSenderId: "208294431387",
+  appId: "1:208294431387:web:096edac2c80e5a5207d43e"
+};
+
+const app = initializeApp(firebaseConfig);
