29449baaf9e5ae5141c5086a198b93d2d01359a7
myosung <rabitmyo@naver.com>
2023-12-06 20:49:04 +0900

    tweet create

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,23 @@
-import { auth } from "../firebase";
+import styled from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
+//import { auth } from "../firebase";
+
+const Wrapper = styled.div``;
+
 
 export default function Home(){
+    /*
     const logOut = () => {
         auth.signOut();
     };
-    return (
-        <h1>
+    <h1>
             <button onClick={logOut}>Log Out</button>
-        </h1>
+    </h1>
+    */
+    return (
+        <Wrapper>
+            <PostTweetForm/>
+        </Wrapper>
+        
     );
 }
